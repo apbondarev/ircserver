12d09b0b722e4d1fff8288eb302cd4464c0550c4
apbondarev <apbondarev@gmail.com>
2016-10-18 22:27:58 +0300

    Print and Echo Servers

---
+++ src/main/java/com/ab/ircserver/EchoServerHandler.java
@@ -0,0 +1,21 @@
+package com.ab.ircserver;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
---
+++ src/main/java/com/ab/ircserver/PrintServerHandler.java
@@ -0,0 +1,28 @@
+package com.ab.ircserver;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+public class PrintServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            // print what it was received
+            System.out.print(in.toString(CharsetUtil.US_ASCII));
+        } finally {
+            in.release();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
