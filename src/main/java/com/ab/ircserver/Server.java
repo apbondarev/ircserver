ea1cbf1ddf7642fd5b866bc2b8a5c1d080a3db51
albondarev <albondarev@localhost.db.com>
2016-10-18 10:44:43 +0300

    initial

---
+++ src/main/java/com/ab/ircserver/Server.java
@@ -0,0 +1,13 @@
+package com.ab.ircserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class Server 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
