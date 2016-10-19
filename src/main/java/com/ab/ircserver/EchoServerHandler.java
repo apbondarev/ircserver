23ebf61d1bd49dd4d909e32e752d90cd002f398d
apbondarev <apbondarev@gmail.com>
2016-10-19 10:24:25 +0300

    Time server

--- src/main/java/com/ab/ircserver/EchoServerHandler.java
+++ src/main/java/com/ab/ircserver/EchoServerHandler.java
@@ -5,17 +5,21 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ctx.write(msg);
-        ctx.flush();
-    }
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		ctx.write(msg);
+	}
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
-        // Close the connection when an exception is raised.
-        cause.printStackTrace();
-        ctx.close();
-    }
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		ctx.flush();
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+		// Close the connection when an exception is raised.
+		cause.printStackTrace();
+		ctx.close();
+	}
 
 }
---
+++ src/main/java/com/ab/ircserver/TimeServerHandler.java
@@ -0,0 +1,31 @@
+package com.ab.ircserver;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		ByteBuf time = ctx.alloc().buffer(4);
+		time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+		ChannelFuture f = ctx.writeAndFlush(time);
+		f.addListener(new ChannelFutureListener() {
+			@Override
+			public void operationComplete(ChannelFuture future) throws Exception {
+				ctx.close();
+			}
+		});
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+		// Close the connection when an exception is raised.
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+}
