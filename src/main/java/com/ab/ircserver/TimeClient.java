5b148164c528aaee1118d554265c5aaf821a92c7
apbondarev <apbondarev@gmail.com>
2016-10-20 11:18:19 +0300

    Time client

---
+++ src/main/java/com/ab/ircserver/TimeClient.java
@@ -0,0 +1,53 @@
+package com.ab.ircserver;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class TimeClient {
+
+	public static void main(String[] args) throws Exception {
+		String host;
+		if (args.length > 0) {
+			host = args[0];
+		} else {
+			host = "localhost";
+		}
+		int port;
+		if (args.length > 1) {
+			port = Integer.parseInt(args[1]);
+		} else {
+			port = 8080;
+		}
+
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+		try {
+			Bootstrap b = new Bootstrap();
+			b.group(workerGroup);
+			b.channel(NioSocketChannel.class);
+			b.option(ChannelOption.SO_KEEPALIVE, true);
+			b.handler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(new TimeClientHandler());
+				}
+			});
+
+			// Start the client
+			ChannelFuture f = b.connect(host, port).sync();
+
+			// Wait until connection is closed
+			f.channel().closeFuture().sync();
+		} finally {
+			workerGroup.shutdownGracefully();
+		}
+
+	}
+
+}
