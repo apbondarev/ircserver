bc0a87ffdb7673b7f279c9a70dd46521d6a0357b
apbondarev <apbondarev@gmail.com>
2017-02-09 22:44:18 +0300

    exception handling

--- src/main/java/com/ab/ircserver/cassandra/DatabaseCassandra.java
+++ src/main/java/com/ab/ircserver/cassandra/DatabaseCassandra.java
@@ -1,6 +1,7 @@
 package com.ab.ircserver.cassandra;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import com.ab.ircserver.Database;
 import com.ab.ircserver.Room;
@@ -11,7 +12,6 @@ import com.datastax.driver.core.QueryOptions;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
 import com.google.common.base.Function;
-import com.google.common.util.concurrent.AsyncFunction;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -19,7 +19,7 @@ import com.google.common.util.concurrent.ListenableFuture;
 public class DatabaseCassandra implements Database {
 
     private Cluster cluster;
-    private ListenableFuture<Session> session;
+    private Session session;
 
     public DatabaseCassandra() {
         connect();
@@ -32,16 +32,11 @@ public class DatabaseCassandra implements Database {
                 .addContactPoint("192.168.1.102")
                 .withQueryOptions(options)
                 .build();
-        session = cluster.connectAsync();
+        session = cluster.connect();
     }
 
     void testConnection() {
-        // Use transform with an AsyncFunction to chain an async operation after another:
-        ListenableFuture<ResultSet> resultSet = Futures.transform(session, new AsyncFunction<Session, ResultSet>() {
-            public ListenableFuture<ResultSet> apply(Session session) throws Exception {
-                return session.executeAsync("select release_version from system.local");
-            }
-        });
+        ListenableFuture<ResultSet> resultSet = session.executeAsync("select release_version from system.local");
 
         // Use transform with a simple Function to apply a synchronous computation on the result:
         ListenableFuture<String> version = Futures.transform(resultSet, new Function<ResultSet, String>() {
@@ -63,9 +58,7 @@ public class DatabaseCassandra implements Database {
         
         try {
             version.get();
-        } catch (RuntimeException e) {
-            throw e;
-        } catch (Exception e) {
+        } catch (ExecutionException | InterruptedException e) {
             throw new IllegalStateException(e);
         }
     }
