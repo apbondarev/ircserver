a426d89770da8172adccc71edb6b53e0e10b7aaa
apbondarev <apbondarev@gmail.com>
2017-02-12 12:18:42 +0300

    cassandra database and test

--- src/main/java/com/ab/ircserver/cassandra/DatabaseCassandra.java
+++ src/main/java/com/ab/ircserver/cassandra/DatabaseCassandra.java
@@ -1,27 +1,43 @@
 package com.ab.ircserver.cassandra;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 import com.ab.ircserver.Database;
+import com.ab.ircserver.Message;
 import com.ab.ircserver.Room;
 import com.ab.ircserver.User;
+import com.datastax.driver.core.BatchStatement;
+import com.datastax.driver.core.CloseFuture;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ConsistencyLevel;
 import com.datastax.driver.core.QueryOptions;
 import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.ResultSetFuture;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SimpleStatement;
+import com.datastax.driver.core.Statement;
 import com.google.common.base.Function;
+import com.google.common.util.concurrent.AsyncFunction;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
 public class DatabaseCassandra implements Database {
 
+    private final String address;
+    private final String keyspace;
     private Cluster cluster;
     private Session session;
 
-    public DatabaseCassandra() {
+    public DatabaseCassandra(String address) {
+        this.address = address;
+        this.keyspace = "ircserver";
         connect();
     }
 
@@ -29,10 +45,10 @@ public class DatabaseCassandra implements Database {
         QueryOptions options = new QueryOptions();
         options.setConsistencyLevel(ConsistencyLevel.LOCAL_QUORUM);
         cluster = Cluster.builder()
-                .addContactPoint("192.168.1.102")
+                .addContactPoint(address)
                 .withQueryOptions(options)
                 .build();
-        session = cluster.connect();
+        session = cluster.connect(keyspace);
     }
 
     void testConnection() {
@@ -62,29 +78,134 @@ public class DatabaseCassandra implements Database {
             throw new IllegalStateException(e);
         }
     }
-
-    @Override
-    public CompletableFuture<User> findOrCreateUser(String name, byte[] password) {
-        return null;
+    
+    void execute(String query) {
+        session.execute(query);
     }
 
     @Override
-    public CompletableFuture<Room> findOrCreateRoom(String roomName) {
-        return null;
+    public CompletionStage<User> findOrCreateUser(String name, byte[] password) {
+        ListenableFuture<ResultSet> resultSet = session.executeAsync("select password from users where name=?", name);
+
+        // Use transform with a simple Function to apply a synchronous computation on the result:
+        ListenableFuture<User> user = Futures.transform(resultSet, new AsyncFunction<ResultSet, User>() {
+            public ListenableFuture<User> apply(ResultSet rs) {
+                if (!rs.isExhausted()) {
+                    Row row = rs.one();
+                    User value = new User(name, row.getBytes("password").array());
+                    return Futures.immediateFuture(value);
+                }
+                String query = "insert into users (name, password) values (?,?) if not exists";
+                Object[] params = new Object[] { name, ByteBuffer.wrap(password) };
+                ListenableFuture<ResultSet> resultSetIfNotExists = session.executeAsync(query, params);
+                return Futures.transform(resultSetIfNotExists, new Function<ResultSet, User>() {
+                    @Override
+                    public User apply(ResultSet rs) {
+                        if (rs.wasApplied()) {
+                            return new User(name, password);
+                        } else {
+                            Row row = rs.one();
+                            return new User(name, row.getBytes("password").array());
+                        }
+                    }
+                });
+            }
+        });
+
+        return buildCompletableFutureFromListenableFuture(user);
     }
 
     @Override
-    public CompletableFuture<Void> save(Room room) {
-        return null;
+    public CompletionStage<Room> findOrCreateRoom(String roomName) {
+        ListenableFuture<ResultSet> resultSet = session.executeAsync("select username, message from rooms where name=? order by ts desc limit 10", roomName);
+
+        // Use transform with a simple Function to apply a synchronous computation on the result:
+        ListenableFuture<Room> user = Futures.transform(resultSet, new AsyncFunction<ResultSet, Room>() {
+            public ListenableFuture<Room> apply(ResultSet rs) {
+                if (!rs.isExhausted()) {
+                    List<Message> messages = new ArrayList<>();
+                    for (Row row : rs.all()) {
+                        messages.add(new Message(row.getString("username"), row.getString("message")));
+                    }
+                    Room value = new Room(roomName, messages);
+                    return Futures.immediateFuture(value);
+                }
+                String query = "insert into rooms (name) values (?) if not exists";
+                ListenableFuture<ResultSet> resultSetIfNotExists = session.executeAsync(query, roomName);
+                return Futures.transform(resultSetIfNotExists, new Function<ResultSet, Room>() {
+                    @Override
+                    public Room apply(ResultSet rs) {
+                        if (rs.wasApplied()) {
+                            return new Room(roomName);
+                        } else {
+                            Row row = rs.one();
+                            return new Room(row.getString("name"));
+                        }
+                    }
+                });
+            }
+        });
+
+        return buildCompletableFutureFromListenableFuture(user);
     }
 
     @Override
-    public CompletableFuture<Void> close() {
-        if (cluster != null) {
-            cluster.close();
-            cluster = null;
+    public CompletionStage<Void> save(Room room) {
+        String query = "insert into rooms (name, ts, username, message) values (?, now(), ?, ?)";
+        
+        BatchStatement batch = new BatchStatement();
+        for (Message m : room.lastMessages()) {
+            Statement st = new SimpleStatement(query, room.name(), m.username(), m.text());
+            batch.add(st);
         }
-        return null;
+        ResultSetFuture resultSet = session.executeAsync(batch);
+        
+        ListenableFuture<Void> result = Futures.transform(resultSet, new Function<ResultSet, Void>() {
+            @Override
+            public Void apply(ResultSet input) {
+                return null;
+            }
+        });
+        
+        return buildCompletableFutureFromListenableFuture(result);
     }
+
+    @Override
+    public CompletionStage<Void> close() {
+        if (cluster != null) {
+            CloseFuture closeFuture = cluster.closeAsync();
+            cluster = null;
+            return buildCompletableFutureFromListenableFuture(closeFuture);
+        }
+        return CompletableFuture.completedFuture(null);
+    }
+    
+    static <T> CompletableFuture<T> buildCompletableFutureFromListenableFuture(  
+        final ListenableFuture<T> listenableFuture) {  
+        //create an instance of CompletableFuture  
+        CompletableFuture<T> completable = new CompletableFuture<T>() {  
+            @Override  
+            public boolean cancel(boolean mayInterruptIfRunning) {  
+                // propagate cancel to the listenable future  
+                boolean result = listenableFuture.cancel(mayInterruptIfRunning);  
+                super.cancel(mayInterruptIfRunning);  
+                return result;  
+            }  
+        };  
+  
+        // add callback  
+        Futures.addCallback(listenableFuture, new FutureCallback<T>() {  
+            @Override  
+            public void onSuccess(T result) {  
+                completable.complete(result);  
+            }  
+  
+            @Override  
+            public void onFailure(Throwable t) {  
+                completable.completeExceptionally(t);  
+            }  
+        });  
+        return completable;  
+    } 
 
 }
--- src/test/java/com/ab/ircserver/cassandra/DatabaseCassandraIT.java
+++ src/test/java/com/ab/ircserver/cassandra/DatabaseCassandraIT.java
@@ -1,18 +1,70 @@
 package com.ab.ircserver.cassandra;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.ab.ircserver.Message;
+import com.ab.ircserver.Room;
+import com.ab.ircserver.User;
+
+import static org.junit.Assert.*;
+
 public class DatabaseCassandraIT {
+    
+    private static DatabaseCassandra db;
+
+    @BeforeClass
+    public static void connect() {
+        db = new DatabaseCassandra("192.168.1.102");
+        db.execute("delete from users where name='user1'");
+        db.execute("delete from rooms where name='room1'");
+    }
+    
+    @AfterClass
+    public static void close() {
+        if (db != null) db.close();
+    }
 
     @Test
     public void testDatabaseCassandra() {
-        DatabaseCassandra db = null;
-        try {
-            db = new DatabaseCassandra();
-            db.testConnection();
-        } finally {
-            if (db != null) db.close();
-        }
+        db.testConnection();
+    }
+    
+    @Test
+    public void findOrCreateUser() throws Exception {
+        // create non-existing user
+        CompletableFuture<User> future = (CompletableFuture<User>) db.findOrCreateUser("name1", "password1".getBytes());
+        User user = future.get();
+        assertEquals("name1", user.name());
+        
+        // find existing user
+        CompletableFuture<User> futureExists = (CompletableFuture<User>) db.findOrCreateUser("name1", "password1".getBytes());
+        User userExists = futureExists.get();
+        assertEquals("name1", userExists.name());
+    }
+    
+    @Test
+    public void findOrCreateRoom() throws Exception {
+        CompletableFuture<Room> future = (CompletableFuture<Room>) db.findOrCreateRoom("room1");
+        Room room = future.get();
+        assertEquals("room1", room.name());
     }
 
+    
+    @Test
+    public void save() throws Exception {
+        List<Message> messages = Arrays.asList(
+                new Message("username1", "text1"),
+                new Message("username2", "text2"),
+                new Message("username3", "text3"));
+        Room room = new Room("room1", messages);
+        CompletableFuture<Void> future = (CompletableFuture<Void>) db.save(room);
+        future.get();
+        assertTrue(future.isDone());
+    }
 }
